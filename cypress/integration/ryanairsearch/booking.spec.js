a0da0d8a0d2312f1391d579f431a535e07a4a717
Sary Navarrete Bejarano <snavarrete@H11013.ad.hugeinc.com>
2020-11-14 20:51:07 -0500

    Booking with git ignore

--- cypress/integration/ryanairsearch/booking.spec.js
+++ cypress/integration/ryanairsearch/booking.spec.js
@@ -25,9 +25,7 @@ describe('Search and book', function(){
         cy.visit('https://www.ryanair.com/gb/en/trip/flights/select?adults=2&teens=0&children=1&infants=0&dateOut=2020-12-06&dateIn=2021-01-02&isConnectedFlight=false&isReturn=true&discount=0&promoCode=&originIata=LIS&destinationIata=BVA&tpAdults=2&tpTeens=0&tpChildren=1&tpInfants=0&tpStartDate=2020-12-06&tpEndDate=2021-01-02&tpDiscount=0&tpPromoCode=&tpOriginIata=LIS&tpDestinationIata=BVA"')
         cy.get('.ng-tns-c35-10').find('.carousel-next').click({force: true})
         cy.get('.ng-trigger-listSlide').contains(' Friday ').click({force: true})
-        //cy.get('[data-ref="2020-12-11"]').contains('11 Dec').click({force: true})
         cy.get('.ng-tns-c33-9').find('.card-header').click({force: true}).wait(300)
-        //cy.get('.fare-card--selected').contains('Continue with Value fare').click({force: true})
         cy.reload().wait(300)
         cy.get('.ng-tns-c35-13').find('.carousel-next').click({force: true})
         cy.get('[data-ref="2021-01-05"]').click({force: true}) 
@@ -41,22 +39,8 @@ describe('Search and book', function(){
             //cy.get('.surname').type('Pereira')
             
            //})
-        
-        
-        //.contains('Continue with Value fare').dblclick({force: true}).wait(300)
-        
-        //cy.get('.login-touchpoint__login-later').should('be.visible')
-    
+     
     })
-  
-    //.find('#svg/takeover-icon').click({force: true})
-    
-    //cy.get('.counter__button-wrapper--enabled').click()
-        
     
 })
 
-
- //.contains('Portugal').click() 
-            //cy.get('#input-button__destination').type('Paris')
-            //cy.get('.airports__airports-list').contains('Lisbon').click()
